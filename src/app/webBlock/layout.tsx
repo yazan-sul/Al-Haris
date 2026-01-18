5cbea9651d3933ce643decb58ae9861a3cca4305
yazan-sul <yazan5sulaiman@gmail.com>
2026-01-18 19:10:26 +0200

    add header in layout

--- src/app/webBlock/layout.tsx
+++ src/app/webBlock/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-
+import { usePathname } from "next/navigation";
 import Sidebar from "@/components/Sidebar/SideBar";
 import { ThemeProvider } from "@/context/ThemeContext";
 
@@ -12,9 +12,23 @@ export default function RootLayout({
 }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const pathname = usePathname();
+
+  const pageTitles: Record<string, string> = {
+    "/dashboard": "لوحة التحكم",
+    "/addchild": "إضافة طفل",
+    "/reports": "التقارير",
+    "/settings": "الإعدادات",
+    "/block": "حظر التطبيقات",
+    "/webBlock": "حظر المواقع",
+  };
+
+  const getPageTitle = () => {
+    return pageTitles[pathname];
+  };
 
   return (
-    <html lang="en" dir="rtl">
+    <html lang="ar" dir="rtl">
       <head />
       <body>
         <ThemeProvider>
@@ -28,11 +42,16 @@ export default function RootLayout({
   bg-white dark:bg-gray-800 
   border-b border-gray-200 dark:border-gray-700 
   transition-all duration-300 ease-in-out 
-  flex items-center "
+  flex items-center px-8"
               >
-                <div className="px-8 py-4"></div>
+                <div className="py-4  flex items-center gap-2">
+                  <span className="text-gray-500 text-base">الرئيسية /</span>
+                  <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
+                    {getPageTitle()}
+                  </h1>
+                </div>
               </div>
-              <main className="flex-1 overflow-auto p-8 bg-white dark:bg-gray-800 flex items-center justify-center">
+              <main className="flex-1 overflow-auto p-8 bg-white dark:bg-gray-800 flex">
                 <div className="w-full max-w-7xl">{children}</div>
               </main>
             </div>
