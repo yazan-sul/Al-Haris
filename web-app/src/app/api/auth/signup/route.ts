277554669994e342d61f5ebaceabd0942d473a65
yazan-sul <yazan5sulaiman@gmail.com>
2026-01-20 22:29:36 +0200

    add error

--- web-app/src/app/api/auth/signup/route.ts
+++ web-app/src/app/api/auth/signup/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
     return NextResponse.json(data, { status: response.status });
   } catch (error) {
     return NextResponse.json(
-      { message: "Internal Server Error" },
+      { message: "Internal Server Error", error },
       { status: 500 },
     );
   }
